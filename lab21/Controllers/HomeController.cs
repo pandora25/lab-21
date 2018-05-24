ae266792236b30e54cb03aad4fd8915f2c7e31ae
MHY Express <mcyalda@yahoo.com>
2018-05-24 16:52:34 -0400

    1st Commit

---
+++ lab21/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab21.Models;
+
+namespace lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SignUpForm()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewUser(UserInfo newUser)
+        {
+            //validations
+            if (ModelState.IsValid)
+            {
+                // insert the new into the db!
+                ViewBag.Message = $"Welcom {newUser.UserName}";
+                return View("Confirm");
+            }
+            else
+            {
+                ViewBag.Address = Request.UserHostAddress;
+                return View("Error");
+            }
+
+        }
+        
+
+    }
+}
---
+++ lab21/Models/UserInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab21.Models
+{
+    public class UserInfo
+    {
+        [Required]
+        public string UserName { set; get; }
+        [Required]
+        public string Password { set; get; }
+        [Required]
+        [RegularExpression(@"/^[a-zA-Z0-9]{5,30}@[a-zA-Z]{5,10}\.[a-zA-Z0-9]{2,3}$",ErrorMessage ="Invalid Email Format")]
+        public string Eamil { set; get; }
+
+        public UserInfo(string userName,string password,string eamil)
+        {
+            UserName = userName;
+            Password = password;
+            Eamil = eamil;
+        }
+
+        public UserInfo()
+        {
+        }
+
+
+
+    }
+}
